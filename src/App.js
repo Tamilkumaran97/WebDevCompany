3c808d2894a0f86bf380f5e30ca9e498d48111c3
Tamilkumaran97 <tamilkumar7070@gmail.com>
2023-05-07 22:55:54 +0530

    first changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Home from "./Webdevcom/Home.js";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./Webdevcom/Navbar.js";
+
+
 function App() {
   return (
     <div>
